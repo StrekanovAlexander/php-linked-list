c792e28ff86ea84ed75bc6a148718ac2e6f122eb
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-11-07 16:14:14 +0200

    rebuild: some functions

--- index.php
+++ index.php
@@ -2,5 +2,13 @@
 require 'pair.php';
 
 $list = makeList(1, 2, 3, 4, 5);
+$list2 = makeList(6, 7, 8);
 
-echo toString($list);
+// echo toString($list);
+// echo '<br>';
+// echo length($list2);
+// echo '<br>';
+// echo append($list, $list2);
+
+$fn = function($x, $acc){return $x + $acc;};
+echo $reduce($fn, $list, 0); 
--- pair.php
+++ pair.php
@@ -47,3 +47,54 @@ function toString($list) {
 
   return $iter($list, '');
 }
+
+function length($list) {
+  if ($list === null) {
+    return 0;
+  } 
+  $iter = function ($l, $acc) use (&$iter) {
+    return $l === null ? $acc : $iter(cdr($l), $acc + 1);
+  };
+  return $iter($list, 0);
+}
+
+function append($list1, $list2) {
+  if ($list2 === null) {
+    return $list1;
+  }
+  if ($list1 === null) {
+    return null;
+  }
+  return append(cdr($list1), cons(car($list1), $list2));  
+}
+
+function sum($list) {
+  $iter = function ($l, $acc) use (&$iter) {
+    if ($l === null) {
+      return $acc;
+    }
+    return $iter(cdr($l), $acc + car($l));
+  };
+
+  return $iter($list, 0);
+}
+
+$map = function ($fn, $list) use (&$map) {
+  if ($list === null) {
+    return null;
+  }
+  $rest = $map($fn, cdr($list));
+  return cons($fn(car($list)), $rest);
+};
+
+$filter = function ($fn, $list) use (&$filter) {
+  if ($list === null) {
+    return null;
+  }
+  $rest = $filter($fn, cdr($list));
+  return $fn(car($list)) ? cons(car($list), $rest) : $rest;
+};
+
+$reduce = function ($fn, $list, $acc) use (&$reduce) {
+  return $list === null ? $acc : $reduce($fn, cdr($list), $fn(car($list), $acc));
+};
